f6d9c6bfdaa6ea8610d11e304f31e57785f7dd27
heurilinares <heurilinares@DESKTOP-7F9G3G4>
2024-02-11 18:43:25 -1200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Número entero");
+Console.WriteLine();
+
+{
+
+    Console.WriteLine("Ingrese la cantidad de números a evaluar:");
+    int cantidadNumeros = Convert.ToInt32(Console.ReadLine());
+
+
+    int sumatoria = 0;
+
+    // Bucle para leer los números y calcular la sumatoria
+    for (int i = 0; i < cantidadNumeros; i++)
+    {
+        Console.WriteLine("Ingrese el número " + (i + 1) + ":");
+        int numero = Convert.ToInt32(Console.ReadLine());
+        sumatoria += numero;
+    }
+
+   
+    if (sumatoria >= 100)
+    {
+        Console.WriteLine("La sumatoria es mayor o igual a 100. Resultado: " + sumatoria);
+    }
+    else
+    {
+        Console.WriteLine("La sumatoria es menor que 100. Resultado: " + sumatoria);
+    }
+
+
+
+}
